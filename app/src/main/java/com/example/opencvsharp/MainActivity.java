7de5cf2e1ab165f20e3fef55f24df813032edfc7
koga3 <shota.koga_weeee@icloud.com>
2023-04-08 19:52:17 +0900

    add: ファイルの作製

---
+++ app/src/main/java/com/example/opencvsharp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.opencvsharp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.opencvsharp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'opencvsharp' library on application startup.
+    static {
+        System.loadLibrary("opencvsharp");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'opencvsharp' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
